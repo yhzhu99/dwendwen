4947c440db2a2e2ced63cad742c4e9edd9d5a56c
Yinghao Zhu <yhzhu99@gmail.com>
2022-02-21 00:35:00 +0800

    feat: add header title and description

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,14 @@
   import SearchBar from "./components/SearchBar.svelte"
 </script>
 
+<svelte:head>
+  <meta charset="UTF-8">
+  <meta name="keywords" content="墩墩起始页,主页,导航,起始页,朱英豪">
+  <meta name="description" content="墩墩起始页由朱英豪开发,整合了多个搜索引擎,便捷好用,一起向未来!">
+  <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no,viewport-fit=cover">
+	<title>墩墩起始页</title>
+</svelte:head>
+
 <div class="font-mono text-2xl mb-5 font-bold text-slate-50 underline decoration-dashed">Together for a Shared Future</div>
 
 <SearchBar/>
