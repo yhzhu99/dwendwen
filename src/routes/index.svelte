b8b4834412dd23b259543c89650109a1c738bb6c
Yinghao Zhu <yhzhu99@gmail.com>
2022-02-20 19:44:16 +0800

    fix: bing dwen dwen typo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,6 @@
   import SearchBar from "./components/SearchBar.svelte"
 </script>
 
-<div class="font-mono text-2xl mb-5 font-bold text-slate-50 underline decoration-dashed">Bing Duen Duen Homepage</div>
+<div class="font-mono text-2xl mb-5 font-bold text-slate-50 underline decoration-dashed">Bing Dwen Dwen Homepage</div>
 
 <SearchBar/>
