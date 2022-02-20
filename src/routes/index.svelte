a0f572c525cfbe2d584faeffaa4ae7ab8f694013
Yinghao Zhu <yhzhu99@gmail.com>
2022-02-20 19:42:01 +0800

    style: add homepage title

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,4 +2,6 @@
   import SearchBar from "./components/SearchBar.svelte"
 </script>
 
+<div class="font-mono text-2xl mb-5 font-bold text-slate-50 underline decoration-dashed">Bing Duen Duen Homepage</div>
+
 <SearchBar/>
