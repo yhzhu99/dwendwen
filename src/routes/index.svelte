adad961db20b9f5a50b143c66933fd04e05fb3ad
Yinghao Zhu <yhzhu99@gmail.com>
2022-02-20 19:14:45 +0800

    fix: browser side rendering

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,5 @@
 <script>
-  import BingDwenDwen from "./components/BingDwenDwen.svelte"
   import SearchBar from "./components/SearchBar.svelte"
 </script>
 
-<main class="flex items-center justify-center min-h-screen bg-sky-500/[.1]">
-  <BingDwenDwen/>
-  <SearchBar/>
-</main>
+<SearchBar/>
